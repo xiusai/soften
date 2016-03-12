6d238a551d621b6e2eedab75c3585a4626bb3645
fasai <fasai_mist@hotmail.com>
2016-03-12 14:52:03 +0700

    create mult func

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
 function add(x,y)
 	{
+		if(isNan(x)) return console.log('input number');
 	return x + y;
 	}
 
 function sub(x,y)
 	{
 	return x - y;
+	}
+	function mul(x,y)
+	{
+	return x *y;
 	}
